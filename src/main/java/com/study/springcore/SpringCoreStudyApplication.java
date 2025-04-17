210b9466ba9cf5d400733dc504c13c5eda65743c
ubing <ubing0101@kakao.com>
2025-04-17 11:47:13 +0900

    initial commit

---
+++ src/main/java/com/study/springcore/SpringCoreStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreStudyApplication.class, args);
+    }
+
+}
